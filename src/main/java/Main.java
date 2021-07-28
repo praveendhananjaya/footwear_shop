0f93b480a24e0772eccd587285d2b3fbb962c2e4
praveen <praveenbhananjaya@gmail.com>
2021-07-28 14:30:40 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+
+/**
+ *
+ */
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+    }
+}
