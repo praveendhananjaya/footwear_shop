f5c7f1c10d840543cd2df06719cb61fabc6145c1
praveen <praveenbhananjaya@gmail.com>
2021-07-28 15:58:51 +0530

    log screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,8 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -16,12 +18,21 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+
+        Parent root = FXMLLoader.load(getClass().getResource("screen/login.fxml"));
+
+        Scene scene = new Scene(root);
+      //  scene.getStylesheets().add(getClass().getResource("theme.css").toExternalForm());
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
